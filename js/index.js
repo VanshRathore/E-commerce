f854a02df1e00e12ccb4e32b1439601bdda4e2b5
Vansh Rathore <73779153+VanshRathore@users.noreply.github.com>
2024-09-18 08:50:30 +0530

    added js in index page

---
+++ js/index.js
@@ -0,0 +1,22 @@
+async function fetchCategories() {
+    const resposne = await fetch("https://fakestoreapi.com/products/categories");
+    const data = await resposne.json();
+    return data;
+}
+
+async function populateCategories() {
+    const categories = await fetchCategories();
+    const categoryList = document.getElementById("category-list");
+    categories.forEach(category => {
+        const categoryHolder = document.createElement("div");
+        const categoryLink = document.createElement("a");
+        categoryLink.href = "#";
+        categoryLink.textContent = category;
+        categoryHolder.classList.add("category-item" ,"d-flex" ,"align-items-center" ,"justify-content-center");
+        categoryHolder.appendChild(categoryLink);
+        categoryList.appendChild(categoryHolder);
+
+    });
+}
+
+populateCategories();
